7e984698f062fd1410c3a8f6f5ce9f0937efabd2
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2021-04-22 20:41:20 -0300

    Create User::Register

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,36 +2,9 @@
 
 class Users::RegistrationsController < ApplicationController
   def create
-    user_params = params.require(:user).permit(:name, :email, :password, :password_confirmation)
-
-    password = user_params[:password].to_s.strip
-    password_confirmation = user_params[:password_confirmation].to_s.strip
-
-    errors = {}
-    errors[:password] = ["can't be blank"] if password.blank?
-    errors[:password_confirmation] = ["can't be blank"] if password_confirmation.blank?
-
-    if errors.present?
-      render_json(422, user: errors)
-    else
-      if password != password_confirmation
-        render_json(422, user: { password_confirmation: ["doesn't match password"] })
-      else
-        password_digest = Digest::SHA256.hexdigest(password)
-
-        user = User.new(
-          name: user_params[:name],
-          email: user_params[:email],
-          token: SecureRandom.uuid,
-          password_digest: password_digest
-        )
-
-        if user.save
-          render_json(201, user: user.as_json(only: [:id, :name, :token]))
-        else
-          render_json(422, user: user.errors.as_json)
-        end
-      end
+    User::Register.call(params: params) do |on|
+      on.success { |result| render_json(201, user: result[:user]) }
+      on.failure { |result| render_json(422, user: result[:errors]) }
     end
   end
 end
